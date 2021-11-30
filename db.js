969cbb52ec367960fb45bc12e52b9ae0c3fe4206
Eli Gabovitch <elirgabovitch@gmail.com>
2021-11-30 15:44:23 -0500

    added environment

--- db.js
+++ db.js
@@ -4,5 +4,5 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 });
-
+console.log(process.env.ENVIRONMENT)
 module.exports = sequelize;
